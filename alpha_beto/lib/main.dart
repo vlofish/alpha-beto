fdc7fab23f1ad81d5611b521e1682b96b8987881
MijaelW@tts <jj9k411pz@mozmail.com>
2022-02-12 13:39:26 -0600

    View Updates Based on Device Orientation

--- alpha_beto/lib/main.dart
+++ alpha_beto/lib/main.dart
@@ -11,20 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Alpha Beto',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: const MyHomePage(title: 'El Alfa-Beto'),
     );
   }
 }
@@ -48,6 +37,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // ignore: unused_field
   int _counter = 0;
 
   void _incrementCounter() {
@@ -63,53 +53,72 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      appBar: AppBar(title: Text(widget.title)),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return Center(
+            child: AlphabetWidget(orientation: orientation),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class AlphabetWidget extends StatelessWidget {
+  const AlphabetWidget({Key? key, required this.orientation}) : super(key: key);
+
+  final Orientation orientation;
+  final MainAxisAlignment centerAlignment = MainAxisAlignment.center;
+
+  @override
+  Widget build(BuildContext context) {
+    return alphabetWidget();
+  }
+
+  Widget alphabetWidget() {
+    const cardsWidgetArray = [CardWidget(), CardWidget()];
+
+    if (orientation == Orientation.portrait) {
+      return Column(
+        mainAxisAlignment: centerAlignment,
+        children: cardsWidgetArray,
+      );
+    }
+
+    return Row(
+      mainAxisAlignment: centerAlignment,
+      children: cardsWidgetArray,
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        splashColor: Colors.orange.withAlpha(30),
+        onTap: () {
+          print('Hello Card Widget.');
+        },
+        child: const SizedBox(
+          width: 100,
+          height: 100,
+          child: Text(
+            'A',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
